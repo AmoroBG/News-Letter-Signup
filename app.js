78aadab000ea8b79671a5b9fa86858ad7bcd6f75
AmoroBG <amorobg@gmail.com>
2021-09-23 09:15:51 -0700

    work on mailchip API

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 const express = require("express")
 const path = require("path")
+const https = require("https")
+
 
 const app = express()
     // Static folder
@@ -15,14 +17,46 @@ app.get("/", function(req, res) {
 })
 
 app.post("/", function(req, res) {
-    let firstname = req.body.firstname
-    let lastname = req.body.lastname
-    let email = req.body.email
+    const firstname = req.body.firstname
+    const lastname = req.body.lastname
+    const email = req.body.email
 
-    res.send(firstname + " " + lastname + " " + email);
+    const data = {
+        members: [{
+            email_address: email,
+            status: "subscribed",
+            merge_fields: {
+                FNAME: firstname,
+                LNAME: lastname
+            }
+        }]
+    }
+    const jsonData = JSON.stringify(data)
+
+    const url = "https://us5.api.mailchimp.com/3.0/7d258239dc"
+    const options = {
+        method: "POST",
+        auth: "IbnSaabs:114c36f6893517b45f600d86a1adf572-us5"
+    }
+
+    const request = https.request(url, options, function(response) {
+
+        response.on("data", function(data) {
+
+        })
+
+    })
+    request.write(jsonData)
+
+
 })
 
 const PORT = 3000 | process.env.PORT
 app.listen(PORT, function() {
     console.log(`Server Started on port ${PORT}`);
 })
+
+
+// MailCHim API key - 114c36f6893517b45f600d86a1adf572-us5
+
+// Audience ID - 7d258239dc
